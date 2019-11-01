d5da747c25a5d8f64784962a5f56e26ec240e28a
GreatScot <greatscot010@gmail.com>
2019-11-01 00:58:24 +0000

    add flask migrate dependency

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
+from flask_migrate import Migrate
 from flaskapp import create_app, db
 
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
+migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
